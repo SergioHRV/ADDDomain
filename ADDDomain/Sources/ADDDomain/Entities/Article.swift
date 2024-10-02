9e186bb15273f5efbbd7e4455dbaeaf0e20824a8
Sergio Ramirez <sh_ramirezv@hotmail.com>
2024-10-02 11:38:40 -0500

    add public init to entity

--- ADDDomain/Sources/ADDDomain/Entities/Article.swift
+++ ADDDomain/Sources/ADDDomain/Entities/Article.swift
@@ -13,4 +13,16 @@ public struct Article {
     public let author: String
     public let createdAt: Date
     public let articleUrl: String
+    
+    public init(id: String, 
+                title: String,
+                author: String,
+                createdAt: Date,
+                articleUrl: String) {
+        self.id = id
+        self.title = title
+        self.author = author
+        self.createdAt = createdAt
+        self.articleUrl = articleUrl
+    }
 }
